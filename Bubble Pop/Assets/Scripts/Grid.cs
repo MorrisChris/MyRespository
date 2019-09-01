e752dccf02d6330abcdea63c33dcc252ba132eb3
MorrisChris <49632304+MorrisChris@users.noreply.github.com>
2019-09-01 16:12:12 -0400

    Started bubble pop
    Created grid and began ball indexing. Next steps are to make a turret and have the balls be random colors.

---
+++ Bubble Pop/Assets/Scripts/Grid.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    public Ball m_Ball;
+
+	// Use this for initialization
+	void Start () {
+		for(int x = -10; x < 11; x++)
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                Ball ball = Instantiate(m_Ball, new Vector3(x, y),new Quaternion(0,0,0,0));
+                ball.m_Index = new Vector2Int(x, y);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
