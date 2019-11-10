b3a164b861237c6639dbf086f1923417fc1ae788
MorrisChris <49632304+MorrisChris@users.noreply.github.com>
2019-11-09 22:07:35 -0500

    Drag added
    Falling objects can now be affected by drag. There are some bugs depending on the shape of the object

--- Artificial Gravity Prototype/Assets/PhysicsBody.cs
+++ Artificial Gravity Prototype/Assets/PhysicsBody.cs
@@ -11,7 +11,7 @@ public class PhysicsBody : MonoBehaviour {
 
     float gravity = 9.81f; //Gravity is 9.81 meters per second
 
-    public float m_Force { get; private set; }
+    public Vector3 m_Weight;
 
     /*
         Drag = -0.5 * C * p * A(v)squared
@@ -40,23 +40,43 @@ public class PhysicsBody : MonoBehaviour {
 
 
 
-        if(m_Collider is CapsuleCollider || m_Collider is SphereCollider)
+        if(m_Collider is SphereCollider)
         {
-            SphereCollider sphereCollider = m_Collider as SphereCollider;
-            float radius = sphereCollider.radius;
-            Area = 4 * Mathf.PI * radius;
+            SphereCollider sphereCollider = GetComponent<SphereCollider>();
+            float radius = transform.localScale.x;
+            Area = Mathf.PI * radius * radius;
         }
-	}
+        else if (m_Collider is CapsuleCollider)
+        {
+            CapsuleCollider capsuleCollider = GetComponent<CapsuleCollider>();
+            float radius = transform.localScale.x;
+            Area = Mathf.PI * radius * radius;
+        }
+        else if(m_Collider is BoxCollider)
+        {
+            BoxCollider boxCollider = GetComponent<BoxCollider>();
+            float length = transform.localScale.x;
+            float width = transform.localScale.z;
+            Area = length * width;
+        }
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 
-        m_Force = m_Mass * (gravity * m_GravityMultiplier);
+        Vector3 gravityForce = new Vector3(0, gravity, 0);
+
+        m_Weight = m_Mass * (gravityForce * m_GravityMultiplier);
 
         Vector3 Velocity = m_RigidBody.velocity;
 
-        m_Drag = -0.5f * DragCoefficient * AirDensity * Area * Velocity;
+        Vector3 VelocitySquared = new Vector3(Velocity.x * Velocity.x,
+            Velocity.y * Velocity.y,
+            Velocity.z * Velocity.z);
 
-        m_RigidBody.AddForce((m_Force * Vector3.down) - m_Drag, ForceMode.Acceleration);
+        m_Drag = 0.5f * DragCoefficient * AirDensity * VelocitySquared * Area;
+
+        m_RigidBody.AddForce((m_Drag - m_Weight), ForceMode.Acceleration);
+        //m_RigidBody.AddForce(-m_Drag, ForceMode.Acceleration);
 	}
 }
